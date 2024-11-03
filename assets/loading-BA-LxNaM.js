641d32d4e29254a1f4d24c737cedabe9bff216ef
阿乐 <1196095012@qq.com>
2024-11-03 21:28:53 +0800

    解决本地资源加载1.3

---
+++ assets/loading-BA-LxNaM.js
@@ -0,0 +1,1 @@
+import{r as t,L as o}from"./index-BmhWhEkA.js";const a=t(0),r=o({get(){return a.value>0},set(e){a.value+=e?1:-1,a.value=Math.max(0,a.value)}});export{r as l};
--- assets/loading-C_6TqRu1.js
+++
@@ -1,1 +0,0 @@
-import{r as t,L as o}from"./index-DGGU6All.js";const a=t(0),r=o({get(){return a.value>0},set(e){a.value+=e?1:-1,a.value=Math.max(0,a.value)}});export{r as l};
