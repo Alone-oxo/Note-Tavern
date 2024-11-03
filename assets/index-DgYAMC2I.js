75042ce7b82dab63e5ac01b2f05abbcaf3bdf42f
阿乐 <1196095012@qq.com>
2024-11-03 21:01:35 +0800

    修复本地资源加载1.1

--- assets/index-DgYAMC2I.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c,e as o,q as t,s as a,h as _}from"./index-Cvt0SBR0.js";const d="/assets/welcome-VFYC1WKN.png",n={},r=e=>(t("data-v-38eb353b"),e=e(),a(),e),p={class:"home card"},i=r(()=>_("img",{class:"home-bg",src:d,alt:"welcome"},null,-1)),l=[i];function m(e,h){return c(),o("div",p,l)}const b=s(n,[["render",m],["__scopeId","data-v-38eb353b"]]);export{b as default};
